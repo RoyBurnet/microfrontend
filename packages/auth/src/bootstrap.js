da0e6317e14a5e6da7bdf5463c638aae1746a17e
Randy <rrw.burnet@gmail.com>
2021-08-22 01:00:34 +0200

    auth routes

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,11 +4,10 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 // Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
-  const history =
-    defaultHistory ||
-    createMemoryHistory({
-      initialEntries: [initialPath],
-    });
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
+
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -17,6 +16,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
+
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 // If we are in development and in isolation, call mount immediatley
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
